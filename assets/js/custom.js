86d531d36f2aa31bdf37c37058511f08d35e93fe
brockfanning <brockfanning@gmail.com>
2020-12-23 15:49:51 +0000

    Deploying to gh-pages from  @ efc165c4e060ba4cd4ebc92b2a83ec1c879b99a0 🚀

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -3,3 +3,11 @@ opensdg.dataRounding = function(value) {
   return value;
   //return Math.round(value * 100) / 100;
 };
+
+opensdg.chartConfigAlter(function(config, info) {
+  // Force the "bar" type if there are less than 2 years of data.
+  if (config.type === 'line' && info.labels.length < 2) {
+    var overrides = {type: 'bar'}
+    $.extend(true, config, overrides);
+  }
+});
